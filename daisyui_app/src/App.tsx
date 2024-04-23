3a12cebac4583ffaed5553a9f2ab6d5ace284ba0
kaneko1117 <yuta_kaneko@pj100.biz>
2024-04-23 17:15:05 +0900

    ✨daisyui

---
+++ daisyui_app/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex space-x-4">
+      <button className="btn btn-ghost">TEXT</button>
+      <button className="btn">CONTAINED</button>
+      <button className="btn btn-outline btn-accent">OUTLINED</button>
+    </div>
+  );
+}
+
+export default App;
