fa07205cacc37b62c1edf287a6d229d5bccf3007
kaneko1117 <yuta_kaneko@pj100.biz>
2024-04-25 18:08:47 +0900

    ✨form

--- daisyui_app/src/App.tsx
+++ daisyui_app/src/App.tsx
@@ -1,10 +1,33 @@
+import { useForm } from "react-hook-form";
+
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<{ name: string }>({
+    mode: "onBlur",
+    defaultValues: { name: "aiful" },
+  });
+  const onSubmit = (data: { name: string }) => {
+    console.log(data);
+  };
   return (
-    <div className="flex space-x-4">
-      <button className="btn btn-ghost">TEXT</button>
-      <button className="btn">CONTAINED</button>
-      <button className="btn btn-outline btn-accent">OUTLINED</button>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="flex flex-col space-y-4 w-[200px]">
+        <input
+          type="text"
+          className="input border border-gray-300 my-[10px]"
+          {...register("name", {
+            required: "入力してください",
+          })}
+        />
+        {errors.name && <p className="text-red-500">{errors.name.message}</p>}
+        <button type="submit" className="btn btn-outline btn-accent">
+          OUTLINED
+        </button>
+      </div>
+    </form>
   );
 }
 
